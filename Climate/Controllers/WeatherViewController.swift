ccf252ba1c4984c8c88f1dc001172ff7285d35ee
Sergey Romanchuk <79969749+hellovidev@users.noreply.github.com>
2021-06-15 12:44:04 +0300

    Initial commit

---
+++ Climate/Controllers/WeatherViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Climate
+//
+//  Created by Sergey Romanchuk on 15.06.21.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
+    }
+
+    @IBAction func searchButtonAction(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Notify when user press return key button
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true
+        } else {
+            searchTextField.placeholder = "Type something here!"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+    
+}
+
